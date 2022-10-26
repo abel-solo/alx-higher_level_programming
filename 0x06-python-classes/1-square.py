8dddaf6cec43b5e3ca8b4950a05b43aa9e699775
abel-solo <abel.solo2011@yahoo.com>
2022-10-26 00:44:40 -0700

    changing the function

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,6 +5,10 @@ class Square:
     Instantiation with size (no type/value verification).
     """
 
-    def __init__(self, size):
-        """Initializes the data."""
+      def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
         self.__size = size

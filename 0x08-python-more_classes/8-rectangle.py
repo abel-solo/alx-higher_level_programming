b7ec151b43d871791b4c8bd3224a81819945b51d
abel-solo <abel.solo2011@yahoo.com>
2022-11-12 05:41:53 -0800

    documentation error solving

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""
+"""
 Contains class Rectangle with private attribute width and height,
 public area and perimeter methods, allows printing using any given symbol,
 deletes, has public attribute to keep track of number of instances,

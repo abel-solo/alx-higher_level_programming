8a499de9f6beb7ca69422f4ec629054534eb4020
abel-solo <abel.solo2011@yahoo.com>
2022-11-27 11:47:17 -0800

    listing tv shows

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked --> https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+
+    SELECT tv_shows.title, tv_show_genres.genre_id
+      FROM tv_shows
+INNER JOIN tv_show_genres
+        ON tv_shows.id = tv_show_genres.show_id
+  ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;

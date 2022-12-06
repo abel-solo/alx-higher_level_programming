92bed853fced86fb0f3bebd97ebfc3058891eeba
abel-solo <abel.solo2011@yahoo.com>
2022-12-05 23:21:23 -0800

    hotfix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,17 +6,21 @@ Contains subclass Rectangle
 with instantiation of private attributes width and height, validated by parent
 """
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
-    """
-    validates width and height values
-    using integer_validator property inherited @ 7-base_geometry
-    Args:
-        width (int): rectangle width
-        height (int): rectangle height
+    """inherits from BaseGeometry
+    Methods:
+        __init__(self, width, height)
     """
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """validate and initialize width and height
+        Args:
+            width (int): private
+            height (int): private
+        """
+        super().integer_validator("width", width)
         self.__width = width
-        self.__height = height 
+        super().integer_validator("height", height)
+        self.__height = height

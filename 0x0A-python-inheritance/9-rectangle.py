4b49e34949cdaeed937f5b1d72db159486a4bf63
abel-solo <abel.solo2011@yahoo.com>
2022-12-05 23:37:00 -0800

    Pycodestyle validation

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,7 +6,6 @@ Contains subclass Rectangle
 with instantiation of private attributes width and height, validated by parent,
 extends parent's area method and prints with __str__
 """
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 

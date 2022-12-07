ac5ec4bc10aa5eacda7a57b77bb0fe922ee28424
abel-solo <abel.solo2011@yahoo.com>
2022-12-07 05:10:56 -0800

    loading adding and saving

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
-"""adds arguments to python list"""
-import sys
+"""
+Contains function that adds and saves to Python obj to JSON file; loads objects
+# run with ./7-add_item.py
+#
+# cat add_item.json ; echo ""
+# expect output: []
+#
+# ./7-add_item.py some random args
+# cat add_item.json ; echo ""
+# expect output: ["some", "random", "args"]
+"""
 
 
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+
 try:
-    items = load_from_json_file(filename)
+    existing_content = load_from_json_file(filename)
 except FileNotFoundError:
-    items = []
-items.extend(sys.argv[1:])
-save_to_json_file(items, filename)
+    existing_content = []
+
+save_to_json_file(existing_content + argv[1:], filename)

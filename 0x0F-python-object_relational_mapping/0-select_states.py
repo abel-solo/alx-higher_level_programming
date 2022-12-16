cd509b34f4f74f8e14c03583b7a82e39046bcf9d
abel-solo <abel.solo2011@yahoo.com>
2022-12-16 12:27:12 -0800

    first commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+script that lists all states from the database hbtn_0e_0_usa
+'''
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    current = db.cursor()
+    current.execute("SELECT * FROM states ORDER BY id")
+    [print(state) for state in current.fetchall()]

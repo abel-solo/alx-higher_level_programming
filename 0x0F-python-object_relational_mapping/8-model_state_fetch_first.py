a8b0291d606ec7eea74654a5331ce8d7f70abd99
abel-solo <abel.solo2011@yahoo.com>
2022-12-17 10:01:02 -0800

    first commit

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+prints the first State object from the database hbtn_0e_6_usa
+arguments: mysql username, mysql password and database name
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_instance = session.query(State).order_by(State.id).first()
+    if first_instance:
+        print("{:d}: {:s}".format(first_instance.id, first_instance.name))
+    else:
+        pass
+
+    session.close()

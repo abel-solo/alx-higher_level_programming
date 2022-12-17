008080612fefaa2ebed6b3a65a149f7f154a654a
abel-solo <abel.solo2011@yahoo.com>
2022-12-17 08:45:59 -0800

    PEP8

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
-    engine001 = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(user, passwd, db), pool_pre_ping=True)
-    Session = sessionmaker(bind=engine001)
+    Session = sessionmaker(bind=engine)
     session = Session()
     # querying python instances in the database
     for instance in session.query(State).order_by(State.id):

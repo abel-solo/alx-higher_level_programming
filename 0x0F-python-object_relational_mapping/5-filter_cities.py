3fdf614b3f2048c9530174382c08737d4c1b7f3e
abel-solo <abel.solo2011@yahoo.com>
2022-12-17 06:53:51 -0800

    cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,17 +3,25 @@
 script takes name of a state as argument and lists all cities of that state
 """
 
+import MySQLdb
+from sys import argv
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                        db=argv[3])
+    cursor = db.cursor()
+    sql_cmd = """SELECT cities.name
+                 FROM states
+                 INNER JOIN cities ON states.id = cities.state_id
+                 WHERE states.name LIKE %s
+                 ORDER BY cities.id ASC"""
+    cursor.execute(sql_cmd, (argv[4], ))
 
-Base = declarative_base()
+    cities = ["{:s}".format(row[0]) for row in cursor.fetchall()]
+    print('- '.join(cities))
 
-
-class State(Base):
-    """
-    Class State; instance of Base
-    """
-    __tablename__ = "states"
-    id = Column(Integer, nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
+    cursor.close()
+    db.close()

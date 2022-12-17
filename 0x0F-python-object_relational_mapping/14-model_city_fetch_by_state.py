5a1204e3a4f1e904d0990ca5f9ff0928bb1e5141
abel-solo <abel.solo2011@yahoo.com>
2022-12-17 11:54:51 -0800

    [featur] printing all the city objects

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,13 +3,11 @@
 contains the class definition of a City
 """
 
-
 from sys import argv
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 
 if __name__ == "__main__":
 

e6f7db085425c8aade68cfda11acb5c4cab41560
abel-solo <abel.solo2011@yahoo.com>
2022-12-17 04:57:28 -0800

    importing the approprait module

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,7 +3,7 @@
 script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument
 """
 
-import argv
+import sis
 import MySQLdb
 
 if __name__ == '__main__':

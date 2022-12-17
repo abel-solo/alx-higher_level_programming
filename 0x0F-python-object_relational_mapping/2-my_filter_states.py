667c9aa7705174e7966a9e16a8664e636f555bbd
abel-solo <abel.solo2011@yahoo.com>
2022-12-17 05:14:11 -0800

    importing the approprait module

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,10 +6,15 @@ script that takes in an argument and displays all values in the states table of 
 import sys
 import MySQLdb
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3], host="localhost")
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], port=3306, host="localhost",
+                         passwd=argv[2], db=argv[3])
     current = db.cursor()
     current.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
     id ASC".format(argv[4]))
     states = current.fetchall()
-    [for state in states print(state) if state[1] == argv[4]] 
+    for state in states:
+        if state[1] == argv[4]:
+            print(state)
+    current.close()
+    db.close()

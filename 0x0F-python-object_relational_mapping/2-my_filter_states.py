f65313f466aa8f02ee44900b9b481524bd94e788
abel-solo <abel.solo2011@yahoo.com>
2022-12-17 05:04:07 -0800

    importing the approprait module

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,10 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], port=3306, passwd=sys.argv[2], db=sys.argv[3], host="localhost")
     current = db.cursor()
     current.execute("SELECT * FROM states WHERE BINARY name = '{}'"
                 .format(sys.argv[4]))
     [print(state) for state in current.fetchall()]
+    current.close()
+    db.close()

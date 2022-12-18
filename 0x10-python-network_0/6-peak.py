8c655a04d329379e5e3e95836d33fcb18c2e28dd
abel-solo <abel.solo2011@yahoo.com>
2022-12-18 07:49:28 -0800

    peak in a list of unsorted int

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Function that finds a peak in a list of unsorted integers
+"""
+
+def find_peak(list_of_integers):
+
+    lint = list_of_integers
+    if lint == []:
+        return None
+    total = len(lint)
+    if total == 1:
+        return lint[0]
+    elif total == 2:
+        return max(lint)
+    middle = int(total / 2)
+    peak = lint[middle]
+    if peak > lint[middle - 1] and peak > lint[middle + 1]:
+        return peak
+    elif peak < lint[middle - 1]:
+        return find_peak(lint[:middle])
+    else:
+        return find_peak(lint[middle + 1:])

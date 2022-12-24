66b2d3cf84092fe708d80ed702d52fc8aa43cf12
abel-solo <abel.solo2011@yahoo.com>
2022-12-24 15:45:49 +0000

    [fix] fix line 7 of importing proper module

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,12 +4,11 @@ python script that takes in a URL, sends a request to the URL and displays the v
 """
 
 from sys import argv
-from urllib.request import Request
-from urllib.request import urlopen
+import urllib.request
 
 if __name__ == "__main__":
     url = argv[1]
 
-    request = Request(url)
-    with urlopen(request) as res:
-        print(dict(response.header).get("x-Request-Id"))
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as res:
+        print(res.getheader("x-Request-Id"))

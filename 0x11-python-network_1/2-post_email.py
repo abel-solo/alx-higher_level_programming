bd933cc488e3fee1c80f4674850f29300aac0db9
abel-solo <abel.solo2011@yahoo.com>
2022-12-24 17:47:24 +0000

    [fix] proper indentation

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,11 +3,14 @@
 script that takes in a URL and email sends a POST request to passed URL
 """
 from sys import argv
-import urllib.parse
-import urllib.request
+from urllib import parse
+from urllib.request import Request, urlopen
 
 if __name__ == '__main__':
-    emaildata = parse.urlencode({'email': argv[2]}).encode('ascii')
-    request = Request(argv[1], emaildata)
-    with urlopen(request) as response:
+    url = argv[1]
+    emaildata = {'email': argv[2]}
+
+    data = urllib.parse.urlencode(emaildata).encode('ascii')
+    req = urllib.request.Request(url,data)
+    with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))

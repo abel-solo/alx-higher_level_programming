433cd95f2393706e7cfe982c35dd5ac6cd170426
abel-solo <abel.solo2011@yahoo.com>
2022-12-24 13:51:20 +0000

    fetching an URL

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """python script that fetches https://alx-intranet.hbtn.io/status"""
-
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response-")
-        print("\t- type - {}".format(type(html)))
-        print("\t- content - {}".format(html))
-        print("\t- utf8 content - {}".format(html.decode('utf-8')))
+    
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+        print('Body response-')
+        print("\t- type - {}".format(type(page)))
+        print("\t- content - {}".format(page))
+        print("\t- utf8 content - {}".format(page.decode('utf-8')))

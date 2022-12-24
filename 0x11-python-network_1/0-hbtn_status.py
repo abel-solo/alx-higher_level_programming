1c0e7411b94a29853825f124fb354f7e1ca3f1e6
abel-solo <abel.solo2011@yahoo.com>
2022-12-24 14:39:55 +0000

    [fix] pycode validation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """python script that fetches https://alx-intranet.hbtn.io/status"""
+
 from urllib.request import *
 
 if __name__ == "__main__":
     
-    url = 'https://alx-intranet.hbtn.io/status'
+    url = "https://alx-intranet.hbtn.io/status"
     req = Request(url)
     with urlopen(req) as response:
         page = response.read()
-    print("Body response-")
-    print("\t- type - {}".format(type(page)))
-    print("\t- content - {}".format(page))
-    print("\t- utf8 content - {}".format(page.decode('utf-8')))
+
+    print("Body response:")
+    print("\t- type: {}".format(type(page)))
+    print("\t- content: {}".format(page))
+    print("\t- utf8 content: {}".format(page.decode('utf-8')))

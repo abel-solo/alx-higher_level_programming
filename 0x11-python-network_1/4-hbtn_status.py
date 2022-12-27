f0d56682c68ee4c70d328cc56c268fff329b8935
abel-solo <abel.solo2011@yahoo.com>
2022-12-27 11:31:19 +0300

    first commit

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ script that fetches https://alx-intranet.hbtn.io/status """
-from requests import get
+
+from requests import *
 
 if __name__ == "__main__":
+
     rs = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(rs.text)))

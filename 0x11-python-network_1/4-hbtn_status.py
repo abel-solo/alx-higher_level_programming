9d26e5218b0fe51032fd94205990152af9c9199b
abel-solo <abel.solo2011@yahoo.com>
2023-01-04 10:57:06 +0300

    [fix] pycodestyle

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """script that fetches https://alx-intranet.hbtn.io/status"""
-import requests
+from requests import get
 
 if __name__ == '__main__':
-
-    res = requests.get('https://alx-intranet.hbtn.io/status')
+    res = get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))

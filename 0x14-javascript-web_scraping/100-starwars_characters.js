8f83c7f437a4d9648a57625af9b0f59623162634
abel-solo <abel.solo2011@yahoo.com>
2023-01-18 19:02:36 +0000

    who was playing the movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,6 +2,7 @@
 const request = require('request');
 const idp = process.argv[2]
 const url = 'https://swapi-api.alx-tools.com/api/films/' + idp;
+
 request(url, function (err, response, content) {
   if (!err) {
     const characters = JSON.parse(content).characters;

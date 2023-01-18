a9fab3ef1e30d3e00cd327b7297e63a98f1ad52d
abel-solo <abel.solo2011@yahoo.com>
2023-01-18 17:28:19 +0000

    gettind content of a webpage

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const endpt = process.argv[2]
+const file = process.argv[3]
+request(endpt).pipe(fs.createWriteStream(file));

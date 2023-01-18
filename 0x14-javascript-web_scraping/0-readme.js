87f909158391b3da3fa9ac18ca4ec662ec9f199c
abel-solo <abel.solo2011@yahoo.com>
2023-01-18 16:13:46 +0000

    [fix] semistandard validation

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const file = require('file');
-const files = process.argv[2]
-file.readFile(files, 'utf-8', function (error, content) {
+const fs = require('fs');
+const file = process.argv[2]
+fs.readFile(file, 'utf-8', function (error, content) {
   console.log(error || content);
 });

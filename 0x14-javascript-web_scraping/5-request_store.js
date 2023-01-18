9401ba2fa855da6950f11e2399a088e822c00e7a
abel-solo <abel.solo2011@yahoo.com>
2023-01-18 17:40:35 +0000

    removing variables

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-let fs = require('fs');
-let request = require('request');
+const fs = require('fs');
+const request = require('request');
 request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));

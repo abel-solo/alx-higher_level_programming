e554037361f2591e45c08e19af7cef075a638b1c
abel-solo <abel.solo2011@yahoo.com>
2023-01-18 18:13:38 +0000

    [fix] adding a proper variable name

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (err, response, body) {
+const endpoint = process.argv[2]
+request(endpoint, function (err, response, body) {
   if (!err) {
-    const tasks = JSON.parse(body);
-    const done = {};
-    tasks.forEach((todo) => {
-      if (todo.done && done[todo.userId] === undefined) {
-        done[todo.userId] = 1;
-      } else if (todo.done) {
-        done[todo.userId] += 1;
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
     });
-    console.log(done);
+    console.log(completed);
   }
 });

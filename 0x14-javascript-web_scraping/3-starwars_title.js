1b943f7b988fdeec47f30e351e2dc08e7b35625f
abel-solo <abel.solo2011@yahoo.com>
2023-01-18 17:18:22 +0000

    [fix] using correct API end point

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let request = require('request');
+const request = require('request');
 const pid = process.argv[2];
-const url = 'http://swapi.co/api/films/' + pid;
+const url = 'https://swapi-api.hbtn.io/api/films/' + pid;
 request(url, function (err, httpResponse, body) {
   console.log(JSON.parse(body).title || err);
 });

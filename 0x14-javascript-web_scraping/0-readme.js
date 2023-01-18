fca2617d40c030254cd58bb6847fa6acfe8eb611
abel-solo <abel.solo2011@yahoo.com>
2023-01-18 09:51:40 +0000

    [fix] removing comment

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-// reads and prints the content of a file
 const fs = require('fs');
 const files = process.argv[2]
+
 fs.readFile(files, 'utf-8', function (error, content) {
   console.log(error || content);
 });

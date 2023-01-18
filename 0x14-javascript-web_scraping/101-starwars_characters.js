dec35a4da5c131d29fa938dcf29f3c23f6956b9e
abel-solo <abel.solo2011@yahoo.com>
2023-01-18 19:10:58 +0000

    [fix] function hoisting

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,7 +4,7 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, (err, res, body) => {
   if (err) throw err;
   const characters = JSON.parse(body).characters;
-  printChars(characters, 0);
+  printCharscter(characters, 0);
 });
 
 function printCharacters (characters, id) {
